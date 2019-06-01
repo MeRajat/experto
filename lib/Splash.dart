ba3991fa94794cb775f67dc897a62d6f2c53121e
maddyb99 <maddyb99@users.noreply.github.com>
2019-06-01 21:32:05 +0530

    added permissions for video and audio

--- lib/Splash.dart
+++ lib/Splash.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class Splash extends StatefulWidget {
   @override
@@ -14,12 +15,30 @@ class SplashState extends State<Splash> {
   void initState() {
     //print(MediaQuery.of(context).size.height);
     //print(MediaQuery.of(context).size.width);
+    getPermissions();
     Future.delayed(Duration(seconds: 2), () {
       Navigator.of(context).pushReplacementNamed('/home_page');
     });
     super.initState();
   }
 
+
+
+  void getPermissions() async{
+    List<PermissionGroup> permission=[PermissionGroup.camera,PermissionGroup.microphone,PermissionGroup.storage];
+    Map<PermissionGroup, PermissionStatus> permissions = await PermissionHandler().requestPermissions(permission);
+    /*permission.forEach((PermissionGroup p)async{
+      PermissionStatus permissionStatus = await PermissionHandler().checkPermissionStatus(p);
+      if(permissionStatus.value==0){
+        bool isShown= await PermissionHandler().shouldShowRequestPermissionRationale(PermissionGroup.contacts);
+        if(!isShown)
+          _ackAlert(context, "Permissions", "The app requires "+p.value.toString()+" permission to function");
+      }
+    });*/
+    permissions.forEach((PermissionGroup pg,PermissionStatus ps){
+      print(pg.toString()+" "+ps.toString()+"\n");
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
