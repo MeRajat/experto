a02bdb6b030bfd2b63e7266184b8194ca4d4cb3f
planetred-cc <pandeyhimanshu709@gmail.com>
2019-06-02 00:44:31 +0530

    merge

--- lib/video_call/init.dart
+++ lib/video_call/init.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:flutter/material.dart';
 /*import io.agora.rtc.Constants;
@@ -25,53 +24,3 @@ class _AgoraState extends State<Agora> {
     return null;
   }
 }
-=======
-import 'package:flutter/material.dart';
-/*
-
-class startVideo extends StatefulWidget{
-  @override
-  _startVideoState createState() => _startVideoState();
-}
-
-class _startVideoState extends State<startVideo> {
-  @override
-  Widget build(BuildContext context) {
-    var =false;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Agora Flutter SDK'),
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            Container(height: 320, child: _viewRows()),
-            OutlineButton(
-              child: Text(=false ? 'Leave Channel' : 'Join Channel',
-                  style: textStyle),
-              onPressed: _toggleChannel,
-            ),
-            Expanded(child: Container(child: _buildInfoList())),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _viewRows() {
-    List<Widget> views = _getRenderViews();
-    if (views.length > 0) {
-      List<Widget> expandeViews = views
-          .map((widget) => Expanded(child: Container(child: widget)))
-          .toList();
-      return Row(children: expandeViews);
-    } else {
-      return null;
-    }
-  }
-
-  List<Widget> _getRenderViews() {
-    return _sessions.map((session) => session.view).toList();
-  }
-}*/
->>>>>>> 52f85a5ec5ddc0f3ac2569d40fdb99e03ffb4580
