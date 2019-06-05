e9f4637f5659511c683750dc82216f62cb309883
MaddyB <madhav.179201109@muj.manipal.edu>
2019-06-05 22:17:26 +0530

    fixed unique mobile number for signup

--- lib/video_call/init.dart
+++ lib/video_call/init.dart
@@ -148,7 +148,7 @@ class _StartVideoState extends State<StartVideo> {
     } else {
       _isInChannel = true;
       AgoraRtcEngine.startPreview();
-      bool status = await AgoraRtcEngine.joinChannel(null, "demo", null, int.parse(currentUser["Mobile"].toString().substring(2)));
+      bool status = await AgoraRtcEngine.joinChannel(null, "notdemo", null, int.parse(currentUser["Mobile"].toString().substring(2)));
       print(status);
     }
     setState(() {});
